4b0667b04b1f86637e01ebd95decf618532a1a9b
Justin Casey <jcasey@pop-os.localdomain>
2020-10-10 12:02:01 -0700

    Changed version from 1.0 to 1.1

--- config.h
+++ config.h
@@ -11,7 +11,7 @@
 #define PACKAGE_NAME "Spotify-CLI"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Spotify-CLI 1.0"
+#define PACKAGE_STRING "Spotify-CLI 1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "spotify-cli"
@@ -20,7 +20,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0"
+#define PACKAGE_VERSION "1.1"
 
 /* Version number of package */
-#define VERSION "1.0"
+#define VERSION "1.1"
